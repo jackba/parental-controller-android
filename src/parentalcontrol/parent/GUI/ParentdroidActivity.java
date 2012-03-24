83ad1ac569f9830af92e8ddd7eb05827bcc55068
ak47gc <ak47gc@gmail.com>
2012-03-24 08:11:05 +0000

    Childroid classes transfer to the parentdroid and change package locations. Add mail service libraries. To do: Remove unwanted classes.

---
+++ src/parentalcontrol/parent/GUI/ParentdroidActivity.java
@@ -0,0 +1,13 @@
+package parentalcontrol.parent.GUI;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParentdroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
